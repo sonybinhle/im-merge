f23aae67c6cf8bbf19e6e2b10e7b3ae39a62e317
Binh Le <sonybinhle@gmail.com>
2018-03-01 23:25:59 +0100

    Add imMerge and set of array modification actions

---
+++ src/imMerge.js
@@ -0,0 +1,100 @@
+const isObject = o => o !== null && typeof o === 'object';
+
+const isEmpty = o => Object.keys(o).length === 0;
+
+const IM_MERGE_TYPE_KEY = '__IM_MERGE_TYPE__';
+
+const MERGE_TYPES = {
+  INSERT: 'INSERT',
+  INSERT_FIRST: 'INSERT_FIRST',
+  INSERT_LAST: 'INSERT_LAST',
+  // INSERT_AFTER_MATCH: 'INSERT_AFTER_MATCH',
+  // INSERT_BEFORE_MATCH: 'INSERT_BEFORE_MATCH',
+  REMOVE: 'REMOVE',
+  REMOVE_FIRST: 'REMOVE_FIST',
+  REMOVE_LAST: 'REMOVE_LAST'
+  // REMOVE_MATCH: 'REMOVE_MATCH'
+};
+
+export const insertType = (data, index, flatten) => ({
+  [IM_MERGE_TYPE_KEY]: MERGE_TYPES.INSERT,
+  data,
+  index,
+  flatten
+});
+
+export const insertFisrtType = (data, flatten) => ({
+  [IM_MERGE_TYPE_KEY]: MERGE_TYPES.INSERT_FIRST,
+  data,
+  flatten
+});
+
+export const insertLastType = (data, flatten) => ({
+  [IM_MERGE_TYPE_KEY]: MERGE_TYPES.INSERT_LAST,
+  data,
+  flatten
+});
+
+export const removeType = (index, flatten) => ({
+  [IM_MERGE_TYPE_KEY]: MERGE_TYPES.REMOVE,
+  index,
+  flatten
+});
+
+export const removeFirstType = (flatten) => ({
+  [IM_MERGE_TYPE_KEY]: MERGE_TYPES.REMOVE_FIRST,
+  flatten
+});
+
+export const removeLastType = (flatten) => ({
+  [IM_MERGE_TYPE_KEY]: MERGE_TYPES.REMOVE_LAST,
+  flatten
+});
+
+const imMerge = (data, source) => {
+  if (Array.isArray(data) && isObject(source) && source[IM_MERGE_TYPE_KEY]) {
+    const {
+      [IM_MERGE_TYPE_KEY]: type,
+      data: sourceData,
+      index = 0,
+      flatten = true
+    } = source;
+
+    const arraySourceData = Array.isArray(sourceData)
+      ? (flatten ? sourceData : [ sourceData ])
+      : [ sourceData ];
+
+    switch (type) {
+      case MERGE_TYPES.INSERT:
+        return [...data.slice(0, index), ...arraySourceData, ...data.slice(index)];
+      case MERGE_TYPES.INSERT_FIRST:
+        return [...arraySourceData, ...data];
+      case MERGE_TYPES.INSERT_LAST:
+        return [...data, ...arraySourceData];
+      case MERGE_TYPES.REMOVE:
+        return [...data.slice(0, index), ...data.slice(index + 1)];
+      case MERGE_TYPES.REMOVE_FIRST:
+        return data.slice(1);
+      case MERGE_TYPES.REMOVE_LAST:
+        return data.slice(0, data.length - 1);
+    }
+  }
+  if (Array.isArray(data) && Array.isArray(source)) {
+    if (!source.length) return data;
+
+    return [...data, ...source];
+  }
+  if (isObject(data) && isObject(source) && !(Array.isArray(data) || Array.isArray(source))) {
+    if (isEmpty(source)) return data;
+
+    return Object.keys(source).reduce((acc, key) => {
+      acc[key] = imMerge(acc[key], source[key]);
+
+      return acc;
+    }, { ...data });
+  }
+
+  return source;
+};
+
+export default imMerge;
---
+++ src/imMerge.test.js
@@ -0,0 +1,204 @@
+import imMerge, {
+  insertType,
+  insertFisrtType,
+  insertLastType,
+  removeType,
+  removeFirstType,
+  removeLastType
+} from './imMerge';
+
+describe('imMerge', () => {
+  it('when merge data with source --> then return new deeply merged object', () => {
+    // given
+    const data = { a: { b: { c: 1, d: 2 }, e: { f: '3' } } };
+    const source = { a: { b: { c: 2, dd: 3 }, ee: { f: '4' } } };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+  });
+
+  it('when merge data with empty source --> then return original object', () => {
+    // given
+    const data = { a: { b: { c: 1, d: 2 }, e: { f: '3' } } };
+    const source = { };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toBe(data);
+  });
+
+  it('when merge data with child empty source --> then return original child object', () => {
+    // given
+    const data = { a: { b: { c: 1, d: 2 }, e: { f: '3' } } };
+    const source = { a: { e: {} } };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj.a.e).toBe(data.a.e);
+    expect(mergedObj.a).not.toBe(data.a);
+  });
+
+  it('when merge data with some non-collision keys --> then non-collision objects will return original references', () => {
+    // given
+    const data = { a: { b: { c: 1, d: 2 }, e: { f: '3' } } };
+    const source = { a: { b: { c: 2 }, e: {} }, d: {} };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj.a.e).toBe(data.a.e);
+    expect(mergedObj.a).not.toBe(data.a);
+    expect(mergedObj.a.b.c).toBe(source.a.b.c);
+    expect(mergedObj.a.b.d).toBe(data.a.b.d);
+    expect(mergedObj.d).toBe(source.d);
+    expect(mergedObj.d).not.toBe(data.d);
+  });
+});
+
+describe('imMerge-array', () => {
+  it('when merge array with insertType source array --> then default index is 0 and return new array with inserted object', () => {
+    // given
+    const data = { a: { b: {}, t: [{ x: 1 }, { x: 2 }] } };
+    const insertedData = { x: 3 };
+    const source = { a: { b: { c: 2 }, t: insertType(insertedData) } };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.a.t[0]).toBe(insertedData);
+    expect(mergedObj.a.t[1]).toBe(data.a.t[0]);
+    expect(mergedObj.a.t[2]).toBe(data.a.t[1]);
+  });
+
+  it('when merge array with insertType and index --> then return new array with inserted object', () => {
+    // given
+    const data = { a: { b: {}, t: [{ x: 1 }, { x: 2 }] } };
+    const insertedData = { x: 3 };
+    const source = { a: { b: { c: 2 }, t: insertType(insertedData, 1) } };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.a.t[0]).toBe(data.a.t[0]);
+    expect(mergedObj.a.t[1]).toBe(insertedData);
+    expect(mergedObj.a.t[2]).toBe(data.a.t[1]);
+  });
+
+  it('when merge array with insertType, index, default flatten --> then return new array with inserted objects', () => {
+    // given
+    const data = { a: { b: {}, t: [{ x: 1 }, { x: 2 }] } };
+    const insertedData = [ { x: 3 }, { x: 4 } ];
+    const source = { a: { b: { c: 2 }, t: insertType(insertedData, 1) } };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.a.t[0]).toBe(data.a.t[0]);
+    expect(mergedObj.a.t[1]).toBe(insertedData[0]);
+    expect(mergedObj.a.t[2]).toBe(insertedData[1]);
+    expect(mergedObj.a.t[3]).toBe(data.a.t[1]);
+  });
+
+  it('when merge array with insertType, index, flatten=false --> then return new array with inserted objects', () => {
+    // given
+    const data = { a: { b: {}, t: [{ x: 1 }, { x: 2 }] } };
+    const insertedData = [ { x: 3 }, { x: 4 } ];
+    const source = { a: { b: { c: 2 }, t: insertType(insertedData, 1, false) } };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.a.t[0]).toBe(data.a.t[0]);
+    expect(mergedObj.a.t[1]).toBe(insertedData);
+    expect(mergedObj.a.t[2]).toBe(data.a.t[1]);
+  });
+
+  it('when merge array with insertFirstType --> then return new array with inserted object', () => {
+    // given
+    const data = { t: [{ x: 1 }, { x: 2 }, { x: 3 }] };
+    const insertedData = { x: 4 };
+    const source = { t: insertFisrtType(insertedData) };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.t[0]).toBe(insertedData);
+    expect(mergedObj.t[1]).toBe(data.t[0]);
+  });
+
+  it('when merge array with insertLastType --> then return new array with inserted object', () => {
+    // given
+    const data = { t: [{ x: 1 }, { x: 2 }, { x: 3 }] };
+    const insertedData = { x: 4 };
+    const source = { t: insertLastType(insertedData) };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.t[2]).toBe(data.t[2]);
+    expect(mergedObj.t[3]).toBe(insertedData);
+  });
+
+  it('when merge array with removeType --> then return new array with object removed', () => {
+    // given
+    const data = { t: [{ x: 1 }, { x: 2 }, { x: 3 }] };
+    const source = { t: removeType(1) };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.t[0]).toBe(data.t[0]);
+    expect(mergedObj.t[1]).toBe(data.t[2]);
+  });
+
+  it('when merge array with removeFirstType --> then return new array with object removed', () => {
+    // given
+    const data = { t: [{ x: 1 }, { x: 2 }, { x: 3 }] };
+    const source = { t: removeFirstType() };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.t[0]).toBe(data.t[1]);
+    expect(mergedObj.t[1]).toBe(data.t[2]);
+  });
+
+  it('when merge array with removeLastType --> then return new array with object removed', () => {
+    // given
+    const data = { t: [{ x: 1 }, { x: 2 }, { x: 3 }] };
+    const source = { t: removeLastType() };
+
+    // when
+    const mergedObj = imMerge(data, source);
+
+    // then
+    expect(mergedObj).toMatchSnapshot();
+    expect(mergedObj.t[0]).toBe(data.t[0]);
+    expect(mergedObj.t[1]).toBe(data.t[1]);
+    expect(mergedObj.t[3]).toBe(undefined);
+  });
+});
